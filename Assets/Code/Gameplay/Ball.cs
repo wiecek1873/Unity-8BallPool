3b8070af4838f554a4922b425efc63a1006370fd
Michał Więcek <wiecek1873@gmail.com>
2023-03-12 00:25:56 +0100

    *Rename

--- Assets/Code/Gameplay/Ball.cs
+++ Assets/Code/Gameplay/Ball.cs
@@ -19,7 +19,7 @@ public class Ball : MonoBehaviour
 
     private void OnDisable()
     {
-        rb.Reset();
+        rb.Stop();
     }
 
     public bool IsMoving()
--- Assets/Code/Utilities/RigidbodyExtensions.cs
+++ Assets/Code/Utilities/RigidbodyExtensions.cs
@@ -4,7 +4,7 @@ namespace Pool.Utilites
 {
     public static class ExtensionsRigidbody
     {
-        public static void Reset(this Rigidbody _rigidbody)
+        public static void Stop(this Rigidbody _rigidbody)
         {
             _rigidbody.velocity = Vector3.zero;
             _rigidbody.angularVelocity = Vector3.zero;
