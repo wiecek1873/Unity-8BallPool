32fe03cd8823d272f960379282a154ab1f6a9b45
Michał Więcek <wiecek1873@gmail.com>
2023-03-11 15:07:09 +0100

    *Method to determine if ball is moving

--- Assets/Code/Ball.cs
+++ Assets/Code/Ball.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    private const float VELOCITY_MIN = 0.1f;
 
+    [SerializeField] private Rigidbody rb;
+
+    public bool IsMoving()
+    {
+        return rb.velocity.magnitude > VELOCITY_MIN;
+    }
 }
--- Assets/Code/Board.cs
+++ Assets/Code/Board.cs
@@ -5,6 +5,19 @@ public class Board : MonoBehaviour, IAutoSetupable
 {
     [SerializeField] private List<Ball> balls = new();
 
+    public bool IsAnyBallMoving()
+    {
+        foreach (Ball _ball in balls)
+        {
+            if (_ball.IsMoving())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public void AutoSetup()
     {
         balls = new();
