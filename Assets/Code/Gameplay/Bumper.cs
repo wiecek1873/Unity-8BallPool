a89422c4ce9efd2b220fdbb7609c399b2a5e7856
Michał Więcek <wiecek1873@gmail.com>
2023-03-12 17:23:25 +0100

    *Script to handle ball collisions

---
+++ Assets/Code/Gameplay/Bumper.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    [SerializeField][Min(0f)] private float bounciness = 0.9f;
+
+    private void OnCollisionEnter(Collision _collision)
+    {
+        Rigidbody _rigidbody = _collision.rigidbody;
+
+        Vector3 _bounceDirection = Vector3.Reflect(_rigidbody.velocity.normalized, _collision.GetContact(0).normal);
+        _rigidbody.velocity = _bounceDirection * Mathf.Max(_rigidbody.velocity.magnitude * bounciness, 0f);
+    }
+}
